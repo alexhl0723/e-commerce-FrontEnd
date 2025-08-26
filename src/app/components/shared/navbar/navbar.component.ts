7bbd6ef43249fdddfd0db0be954016ace5f08d86
jetlixd24 <huanacodelacruz@gmail.com>
2025-08-26 00:36:56 -0500

    actualizacion de funcion de roles

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,18 +1,40 @@
-import { Component } from '@angular/core';
+// navbar.component.ts
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../../../services/auth/auth.service'
+import { AuthService } from '../../../services/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
   isMobileMenuOpen = false;
+  isUserAdmin: boolean = false;
+  isUserClient: boolean = false;
+  
+  // SOLUCIÓN: Inicializar la propiedad a null
+  private authSubscription: Subscription | null = null;
 
-  constructor(private authService: AuthService ){}
+  constructor(public authService: AuthService) { }
 
+  ngOnInit(): void {
+    // Aquí es donde le asignas un valor
+    this.authSubscription = this.authService.userRole$.subscribe(roleId => {
+      this.isUserAdmin = roleId === 1;
+      this.isUserClient = roleId === 2;
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Es importante verificar si la suscripción existe antes de cancelarla
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
@@ -22,8 +44,7 @@ export class NavbarComponent {
     this.isMobileMenuOpen = false;
   }
 
-  cerrarSession(){
-    this.authService.logout()
+  logout() {
+    this.authService.logout();
   }
-
 }
